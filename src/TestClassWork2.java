37146a2aeeb167bb95630348f08a50fd9d75d07a
tzdorenko <tanyazdorenkoo@gmail.com>
2023-01-08 21:35:13 +0200

    Add new work 08/01/23

--- src/TestClassWork2.java
+++ src/TestClassWork2.java
@@ -1,2 +1,51 @@
 public class TestClassWork2 {
+//    static void TestMethod(){
+//        System.out.println("HelloWorld");
+//    }
+//    static void TwoVariables(){
+//        int a = 8;
+//        int b = 125;
+//        int c = Math.min(a, b);
+//        System.out.println(c);
+//    }
+static void Min(int a, int b){
+    if (a < b) {
+        System.out.println(a);
+    } else {
+        System.out.println(b);
+    }
 }
+
+
+    public static void main(String[] args) {
+//        TestMethod();
+//        TwoVariables();
+        Min(3, 15);
+    }
+//    public static void main(String[] args) {
+//int [][] one = {
+//        {1, 5, 7},
+//        {8, 10, 4},
+//        {13, 9, 1}
+//};
+//int [][] two = {
+//        {4, 3, 8},
+//        {5, 1, 15},
+//        {8, 2, 7}
+//};
+//int [][] result = new int[3][3];
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result.length; j++) {
+//                result [i][j] = one [i][j] + two [i][j];
+//            }
+//        }
+//        for (int i = 0; i < result.length; i++) {
+//            for (int j = 0; j < result.length; j++) {
+//                System.out.print(result[i][j] + " ");
+//            }
+//            System.out.println();
+//                    }
+//        }
+    }
+
+
