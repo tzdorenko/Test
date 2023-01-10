c8398ed770843e816adbbebd6506e9a32bf225b4
tzdorenko <tanyazdorenkoo@gmail.com>
2023-01-10 11:00:11 +0200

    Calculator (+,-,*,/,%, degree)

--- src/HomeWork/Three.java
+++ src/HomeWork/Three.java
@@ -1,4 +1,49 @@
 package HomeWork;
 
+import java.util.Scanner;
+
 public class Three {
+    public static void main(String[] args) {
+        char operator = 0;
+        double num1, num2, result = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first number");
+        num1 = input.nextDouble();
+        System.out.println("Enter the operator: +, -, *, /, %");
+        operator = input.next().charAt(0);
+        System.out.println("Enter second number");
+        num2 = input.nextDouble();
+//        int devAndRemOne, devAndRemTwo, result1;
+//        devAndRemOne = input.nextInt();
+//        devAndRemTwo = input.nextInt();
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println(num1 + "*" + num2 + " = " + result);
+                break;
+            case '/':
+                result = num1 / num2;
+                System.out.println(num1 + "/" + num2 + " = " + result);
+                break;
+            case '^':
+                result = Math.pow(num1, num2);
+                System.out.println(" degree = " + result);
+                break;
+            case '%':
+                result = num1 % num2;
+                System.out.println("division with a remainder = " + result);
+                break;
+            default:
+                System.out.println("Wrong format");
+
+        }
+    }
 }
--- src/NewClassPrivateOne.java
+++ src/NewClassPrivateOne.java
@@ -1,2 +1,8 @@
 public class NewClassPrivateOne {
+    public static void main(String[] args) {
+        int a = 290;
+        int b = 7;
+        int c = 0;
+        System.out.println(c=a%b);
+    }
 }
