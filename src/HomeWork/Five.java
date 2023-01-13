ab04e8846e44bbcc8a34cc52ddee6ad1dc2e51b0
tzdorenko <tanyazdorenkoo@gmail.com>
2023-01-13 14:58:17 +0200

    ))

--- src/HomeWork/Five.java
+++ src/HomeWork/Five.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Five {
     public static void main(String[] args) {
+String [] days = {"Monday", "Tuesday", "Wednesday"};
+        System.out.println(days[2]);
 
+        int[] MyNum = new int[]{12, 65, 876};
+        System.out.println(MyNum[1]);
+
+        double [] Prices = {12.5, 44.6, 87.45};
+        Prices [1] = 33.22;
+        System.out.println(Prices[1]);
     }
 }
